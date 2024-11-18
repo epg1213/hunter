e17c778a8ea1c4d6bfce606cbc6ca578e87cf5ed
epg1213 <epg1213@gmail.com>
2024-11-18 20:13:13 +0100

    fixing minor issue

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ fi
 
 DBPASS=$(tr -dc 'A-Za-z0-9!#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 16)
 echo $DBPASS > .dbpass
-sudo docker stop hunter-mysql
-sudo docker rm hunter-mysql
+sudo docker stop hunter-mysql 2>/dev/null
+sudo docker rm hunter-mysql 2>/dev/null
 sudo docker run --name hunter-mysql -p 42601:3306 -e MYSQL_ROOT_PASSWORD=$DBPASS -d mysql:latest
