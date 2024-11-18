fa55e3db089d264a23017a653e271dd0c41c6762
epg1213 <epg1213@gmail.com>
2024-11-18 19:43:17 +0100

    docker installation

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 /usr/bin/python3 -m venv venv
 source ./venv/bin/activate
 pip3 install -r ./requirements.txt
+
+sudo apt install curl
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
