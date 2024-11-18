9558bab48983ebdecc133cdd40c6f06786cbc434
epg1213 <epg1213@gmail.com>
2024-11-18 19:50:23 +0100

    checking ifdocker is installed

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 source ./venv/bin/activate
 pip3 install -r ./requirements.txt
 
-sudo apt install curl
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+if ! docker --version; then
+	echo "Docker doesn't seem to be installed"
+    sudo apt install curl
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+fi
