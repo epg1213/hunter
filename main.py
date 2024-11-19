bcd2f369b216cd183630d2591731a0f8b89f9e76
epg1213 <epg1213@gmail.com>
2024-11-19 14:23:34 +0100

    hello world with flash

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from models.crawler import Crawler
+"""from models.crawler import Crawler
 
 url="https://hackazon.trackflaw.com"
 crawler = Crawler()
 crawler.crawl(url)
-print(crawler.visited)
+print(crawler.visited)"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+app.run(host='0.0.0.0', port=42601)
