a874c828bfc7aad79f0a79a0bdada3ae1a300fea
epg1213 <epg1213@gmail.com>
2024-11-19 12:55:05 +0100

    fixing minor issues

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ echo $DBPASS > .dbpass
 sudo docker stop hunter-mysql
 sudo docker rm hunter-mysql
 sudo docker run --name hunter-mysql -p 42601:3306 -e MYSQL_ROOT_PASSWORD=$DBPASS -e MYSQL_DATABASE=hunting -d mysql:latest
+sleep 5
 
 echo "Preparing management script \"hunter\"..."
 chmod +x hunter
