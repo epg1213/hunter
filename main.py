d148056ec2922b08aa25e503478d5d92795e00fb
epg1213 <epg1213@gmail.com>
2024-11-24 15:01:59 +0100

    form for creation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ crawler = Crawler()
 crawler.crawl(url)
 print(crawler.visited)"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import logging
 from models.api import *
 
@@ -13,8 +13,10 @@ app = Flask(__name__)
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST' and 'name' in request.form:
+        create_project(request.form['name'])
     return render_template('index.html', projects=get_projects())
 
 app.run(host='0.0.0.0', port=42602)
--- models/api.py
+++ models/api.py
@@ -1,5 +1,9 @@
 from models.database import DataBase
+import datetime
 db=DataBase()
 def get_projects():
     return db.request("SELECT * FROM project", ())
 
+def create_project(name):
+    date=datetime.datetime.now()
+    return db.request("INSERT INTO project(name, creation_date) VALUES(%s, %s)", (name, date))
