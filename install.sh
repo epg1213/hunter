121b1c82fa922f10a05e0de5ee4f444dbeeed8db
epg1213 <epg1213@gmail.com>
2025-01-14 03:41:06 +0100

    sql container set up

--- install.sh
+++ install.sh
@@ -14,6 +14,15 @@ sudo docker remove hunter-sql
 sudo docker image rm hunter-sql-img
 sudo docker build -t hunter-sql-img .
 sudo docker run --name hunter-sql -d -p $SQLPORT:3306 -e MYSQL_ROOT_PASSWORD=$SQLPASS -e MYSQL_DATABASE=hunting hunter-sql-img
+echo "Waiting for database to be healthy..."
+while ! docker exec hunter-sql mysql --user=root --password=$SQLPAS -e "status" &> /dev/null ; do
+    echo -n "."
+    sleep 1
+done
+sudo docker exec -d hunter-sql /bin/sh -c "mysql -u root -p$SQLPASS </db_setup/page.sql"
+sudo docker exec -d hunter-sql /bin/sh -c "mysql -u root -p$SQLPASS </db_setup/parameter.sql"
+sudo docker exec -d hunter-sql /bin/sh -c "mysql -u root -p$SQLPASS </db_setup/project.sql"
+sudo docker exec -d hunter-sql /bin/sh -c "mysql -u root -p$SQLPASS </db_setup/website.sql"
 cd ..
 echo "Setting up Web container..."
 cd web-container
@@ -23,9 +32,3 @@ sudo docker image rm hunter-web-img
 sudo docker build -t hunter-web-img .
 sudo docker run --name hunter-web -d -p $WEBPORT:80 -e MYSQL_PORT=$SQLPORT -e MYSQL_ROOT_PASSWORD=$SQLPASS hunter-web-img
 cd ..
-sleep 5
-echo "Setting up database..."
-sudo docker exec -d hunter-sql /bin/sh -c "mysql -u root -p$SQLPASS </db_setup/page.sql"
-sudo docker exec -d hunter-sql /bin/sh -c "mysql -u root -p$SQLPASS </db_setup/parameter.sql"
-sudo docker exec -d hunter-sql /bin/sh -c "mysql -u root -p$SQLPASS </db_setup/project.sql"
-sudo docker exec -d hunter-sql /bin/sh -c "mysql -u root -p$SQLPASS </db_setup/website.sql"
